1cc5da66cc7d2ffab7618c9c3ecbd8fa6708cfe2
aungthuwin-luwis <165756555+aungthuwin-luwis@users.noreply.github.com>
2024-08-18 16:18:34 +0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,11 +4,13 @@
 # Create SubNet
 # Create RTB
 
+#data view (Zone state)
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
 
+#Create VPC
 resource "aws_vpc" "My_Lab_VPC" {
     cidr_block = var.my_vpc_cidr
     tags = {
@@ -17,6 +19,7 @@ resource "aws_vpc" "My_Lab_VPC" {
   
 }
 
+#Create IGW
 resource "aws_internet_gateway" "My_Lab_IGW" {
     tags = {
       Name = "My_Lab_IGW"
@@ -24,12 +27,14 @@ resource "aws_internet_gateway" "My_Lab_IGW" {
   
 }
 
+#Create IGW Attach
 resource "aws_internet_gateway_attachment" "My_GW_attach" {
   vpc_id = aws_vpc.My_Lab_VPC.id
   internet_gateway_id = aws_internet_gateway.My_Lab_IGW.id
 }
 
 
+#Create Route Table
 resource "aws_route_table" "My_Lab_RTB" {
   vpc_id = aws_vpc.My_Lab_VPC.id
 
@@ -43,6 +48,7 @@ resource "aws_route_table" "My_Lab_RTB" {
 
 }
 
+#Create Subnet
 resource "aws_subnet" "My_Lab_Sub01" {
     count = length(data.aws_availability_zones.available.names)
     vpc_id = aws_vpc.My_Lab_VPC.id
@@ -54,6 +60,7 @@ resource "aws_subnet" "My_Lab_Sub01" {
       
 }
 
+#Create Route Table Association
 resource "aws_route_table_association" "My_pub_rtb" {
     count = length(aws_subnet.My_Lab_Sub01)
   route_table_id = aws_route_table.My_Lab_RTB.id
