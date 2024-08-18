6335c3a072cef7936cc7cb9cad635f9d47edaf64
aungthuwin-luwis <165756555+aungthuwin-luwis@users.noreply.github.com>
2024-08-18 16:15:33 +0700

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "name_vpc" {
+    default = "My_Lab_VPC"
+  
+}
+
+variable "my_vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "zone_for_subnet" {
+  type = list(string)
+  default = [ "ap-southeast-1a" , "ap-southeast-1b" ,"ap-southeast-1c"]
+}
+
+variable "cidr_for_subnet" {
+  type = list(string)
+  default = [ "10.0.1.0/24" , "10.0.2.0/24" ,"10.0.3.0/24"]
+}
+
+variable "name_for_subnet" {
+  type = list(string)
+  default = [ "My_Lab_Sub01" , "My_Lab_Sub02" ,"My_Lab_Sub03"]
+}
